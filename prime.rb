6c6d2468b12e32668c4043308005b3d597e0451a
Jesse Reeves <reeveswriting@gmail.com>
2019-04-07 21:23:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
-def prime?(number)
-  if number < 2 
-    return FALSE
-end
-
-(2..n-1).each do |number|
-   if n % number == 0
-
-  return false
- end
-end
- return true
+def prime?(num)
+  return false if num <= 1
+  for i in 2..(num/2)
+    return false if num % i == 0
+  end
+  true
 end
